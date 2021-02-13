fc972844ffdd5f782eeda75537a155b8de161209
Illustrova <Illustrova@users.noreply.github.com>
2021-02-13 22:38:21 +0000

    deploy: c9cd32b1310491140a9ec71bb4161b1e70d58dd2

---
+++ _next/static/zonAw0S9GIwS5MOmqwxhb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-4d626cfdde8ced28b745.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-05b44a3e5b2abd6f33a8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
